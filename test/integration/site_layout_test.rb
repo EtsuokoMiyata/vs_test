e301e99415bc2b5af9616b66f1de6a8ea0dcfc95
Etsuko Miyata <yatayata1018@gmail.com>
2018-09-26 13:41:06 +0000

    演習問題２個修正

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,9 +8,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    get contact_path
-    assert_select "title", full_title("Contact")
-    get signup_path
-    assert_select "title", full_title("Sign up")
+    #以下　slackより演習問題は削除
+    #get contact_path
+    #assert_select "title", full_title("Contact")
+    #get signup_path
+    #assert_select "title", full_title("Sign up")
   end
 end
