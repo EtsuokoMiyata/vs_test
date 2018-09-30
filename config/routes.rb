42bba5bab3aca3384d855013be593c9946b341b8
Etsuko Miyata <yatayata1018@gmail.com>
2018-09-30 06:30:47 +0000

    8.2.5

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
-  #post '/signup',  to: 'users#create'            #7.3.4　演習2のためコメントアウト　テストはOK
+  post '/signup',  to: 'users#create'            #7.3.4　演習2のためコメントアウト　テストはOK
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
