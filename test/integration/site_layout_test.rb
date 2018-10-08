e748a3a220b00dd690fa182a05fe384ac8a07136
Etsuko Miyata <yatayata1018@gmail.com>
2018-10-08 00:57:01 +0000

    10.3.1 + 演習1

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,10 +8,28 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    #以下　slackより演習問題は削除
-    #get contact_path
-    #assert_select "title", full_title("Contact")
-    #get signup_path
-    #assert_select "title", full_title("Sign up")
+    
+    #以下　演習問題でslack情報より一旦削除したが　test通ったので復活
+    get contact_path                              #演習問題
+    assert_select "title", full_title("Contact")  #演習問題
+    get signup_path                               #演習問題
+    assert_select "title", full_title("Sign up")  #演習問題
   end
+  
+  #以下　演習問題10.3.1.1 ログイン済みユーザーとそうでないユーザーのそれぞれに対して、正しい振る舞い
+  def setup
+    @user       = users(:michael)
+  end
+  
+  test "layout links when logged in" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)    
+    assert_select "a[href=?]", logout_path
+  end
+  
+  
 end
