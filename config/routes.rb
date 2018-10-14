12970745028baaef32679450c09b19d29818c067
Etsuko Miyata <yatayata1018@gmail.com>
2018-10-14 00:14:28 +0000

    11.1.1

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup', to: 'users#new'
-  post '/signup',  to: 'users#create'            #7.3.4　演習2のためコメントアウト　テストはOK
+  post '/signup',  to: 'users#create'     
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  
   resources :users
+  resources :account_activations, only: [:edit]
 end
