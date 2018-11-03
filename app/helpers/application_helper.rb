f4027c161173350e1af0cb8f755c60f4b8e88fbc
Etsuko Miyata <yatayata1018@gmail.com>
2018-11-03 13:22:57 +0000

    日本語化件名 RED

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  require "date"   #Dateクラスを使えるようにする
+  def now_time
+    @now = Date.today  #Date.today(今日の日付)を変数todayに代入する
+  end
 
   # ページごとの完全なタイトルを返します。                   # コメント行
   def full_title(page_title = '')                     # メソッド定義とオプション引数
