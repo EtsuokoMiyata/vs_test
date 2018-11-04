fe359442e59dbfd73ee08d0e652de10a75f873fa
Etsuko Miyata <yatayata1018@gmail.com>
2018-11-04 03:47:11 +0000

    ユーザー検索

--- app/models/user.rb
+++ app/models/user.rb
@@ -101,6 +101,15 @@ class User < ApplicationRecord
     following.include?(other_user)
   end
   
+  #ユーザー検索拡張機能
+  def self.search(search) #ここでのself.はUser.を意味する
+    if search
+      where(['name LIKE ?', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
+    else
+      all #全て表示。User.は省略
+    end
+  end
+  
    private
 
     # メールアドレスをすべて小文字にする
