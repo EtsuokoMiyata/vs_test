37e3b78e7eb54171afb6e1d7a3259ab96d122b6e
Etsuko Miyata <yatayata1018@gmail.com>
2018-11-07 13:36:55 +0000

    正規表現訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,10 @@ class User < ApplicationRecord
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name,  presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  #下記は　当初のもの
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  #下記は、訂正したもの　2018/11/07
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
