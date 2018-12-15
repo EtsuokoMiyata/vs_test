4fd78c8b21c02ffcef84363c40505316fe64f9cd
Etsuko Miyata <yatayata1018@gmail.com>
2018-12-15 00:20:14 +0000

    20181212130829_remove_worktime_from_users.rb

--- db/migrate/20181212130829_remove_worktime_from_users.rb
+++ db/migrate/20181212130829_remove_worktime_from_users.rb
@@ -1,6 +1,6 @@
 class RemoveWorktimeFromUsers < ActiveRecord::Migration[5.1]
   def change
-    remove_column :users, :fixed_work_time, :time
-    remove_column :users, :basic_work_time, :time
+    remove_column :users, :fixed_work_time, :float
+    remove_column :users, :basic_work_time, :float
   end
 end
